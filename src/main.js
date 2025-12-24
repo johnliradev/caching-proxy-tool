72b36439a447de4dc22b25f6bba912b0b5c3e83d
John <johnalsoares@gmail.com>
2025-12-24 15:17:28 -0300

    Add basic HTTP server implementation in main.js

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import { createServer } from "node:http";
+
+const hostname = "localhost";
+const port = process.env.PORT;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
